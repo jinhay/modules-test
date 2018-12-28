fcdb0d3f00d5f4d7f5877c088d211686ab117f60
“your_ujinhay <210888922@qq.com>
2018-12-28 16:54:52 +0800

    1

--- src/ModulesTestServerProvider.php
+++ src/ModulesTestServerProvider.php
@@ -13,17 +13,11 @@ class ModulesTestServerProvider extends ServiceProvider
      */
     public function boot()
     {
-        if ($this->app->runningInConsole()) {
-//            $this->publishes([__DIR__.'/../config' => config_path()], 'laravel-admin-config');
-//            $this->publishes([__DIR__.'/../resources/lang' => resource_path('lang')], 'laravel-admin-lang');
-//            $this->publishes([__DIR__.'/../resources/views' => resource_path('views/vendor/admin')],           'laravel-admin-views');
-            //->publishes([__DIR__.'/../database/migrations' => database_path('migrations')], 'laravel-admin-migrations');
-            //$this->publishes([__DIR__.'/../resources/assets' => public_path('vendor/laravel-admin')], 'laravel-admin-assets');
-//            $this->publishes([__DIR__.'/../controller' => base_path('/app/Http/Controller'),'laravel-admin-controller']);
+
             $this->publishes([__DIR__.'/../Modules'=>app_path("Modules"),"modules"]);
             $this->publishes([__DIR__.'/../view'=>resource_path('views')],"modules-view");
             $this->publishes([__DIR__.'/../config/test.php'=>config_path('test.php')],'modules-config');
-        }
+
 
     }
 
