029bb37af62646d4547e30f7ea4e27dda492d40b
“your_ujinhay <210888922@qq.com>
2018-12-28 21:49:15 +0800

    1

--- Modules/Test/Controllers/TestController.php
+++ Modules/Test/Controllers/TestController.php
@@ -10,12 +10,14 @@ namespace Modules\Test\Controllers;
 
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 
 class TestController extends Controller
 {
     public function index()
     {
 //        dump( config() );
-            return view(config("test.modules_view")."index");
+        $users = DB::select('select * from admin_users where 1');
+        return view(config("test.modules_view")."index",['user'=>$users]);
     }
 }
